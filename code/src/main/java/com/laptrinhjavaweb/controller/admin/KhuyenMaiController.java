0f15de52208bef2ebb54156b1b308a980a1d3161
huydqph27044 <huydqph27044@fpt.edu.vn>
2023-10-05 15:38:50 +0700

    huy test

---
+++ code/src/main/java/com/laptrinhjavaweb/controller/admin/KhuyenMaiController.java
@@ -0,0 +1,12 @@
+package com.laptrinhjavaweb.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/khuyen-mai")
+public class KhuyenMaiController {
+
+
+
+}
