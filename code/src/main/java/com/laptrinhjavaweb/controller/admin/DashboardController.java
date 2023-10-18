12cd10f79a2e23235f142d434a53adc96c846930
TUNTINTI-03\asus <tuandaph19896@fpt.edu.vn>
2023-10-18 16:58:17 +0700

    update gd profile

--- code/src/main/java/com/laptrinhjavaweb/controller/admin/DashboardController.java
+++ code/src/main/java/com/laptrinhjavaweb/controller/admin/DashboardController.java
@@ -53,11 +53,6 @@ public class DashboardController {
         return "admin/profile";
     }
 
-    @GetMapping("/setting_profile")
-    public String setting_profile(){
-        return "admin/setting_profile";
-    }
-
     @GetMapping("/setting_banner")
     public String setting_banner(){
         return "admin/banner";
