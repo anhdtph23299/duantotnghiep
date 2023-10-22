17eaaba4e26db90b7315188da908cf03a6975013
dnthanhstudy <dangngocthanhstudy@gmail.com>
2023-10-23 03:28:11 +0700

    Additional RESTFUL API for products and fix-bug basic in front-end

--- src/main/java/com/laptrinhjavaweb/api/SanPhamHinhAnhAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/SanPhamHinhAnhAPI.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,8 @@ public class SanPhamHinhAnhAPI {
 
 	@Autowired
 	private ISanPhamHinhAnhService sanPhamHinhAnhService;
-	 
+
+	@PostMapping
 	public ResponseEntity<?> createdHinhAnh(@RequestBody List<SanPhamHinhAnhDTO> sanPhamHinhAnhsDTO) {
 		List<SanPhamHinhAnhDTO> results = sanPhamHinhAnhService.save(sanPhamHinhAnhsDTO);
 		return new ResponseEntity<>(results, HttpStatus.CREATED);
