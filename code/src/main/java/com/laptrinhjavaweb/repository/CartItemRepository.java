2b9e16553696b443fc778d730a9644beddbda39c
huydqph27044 <huydqph27044@fpt.edu.vn>
2023-10-26 00:56:04 +0700

    bug

--- code/src/main/java/com/laptrinhjavaweb/repository/CartItemRepository.java
+++ code/src/main/java/com/laptrinhjavaweb/repository/CartItemRepository.java
@@ -3,5 +3,6 @@ package com.laptrinhjavaweb.repository;
 import com.laptrinhjavaweb.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
