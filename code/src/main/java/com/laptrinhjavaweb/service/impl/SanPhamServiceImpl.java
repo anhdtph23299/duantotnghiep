0ddadddcaca867f2d4fa57f51b577a0770226821
dnthanhstudy <dangngocthanhstudy@gmail.com>
2023-10-26 18:50:28 +0700

    Add trang-chu, detail

--- code/src/main/java/com/laptrinhjavaweb/service/impl/SanPhamServiceImpl.java
+++ code/src/main/java/com/laptrinhjavaweb/service/impl/SanPhamServiceImpl.java
@@ -1,20 +1,21 @@
 package com.laptrinhjavaweb.service.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.laptrinhjavaweb.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.laptrinhjavaweb.converter.SanPhamConverter;
 import com.laptrinhjavaweb.converter.ThuocTinhConverter;
-import com.laptrinhjavaweb.dto.BienTheDTO;
-import com.laptrinhjavaweb.dto.GiaTriThuocTinhDTO;
-import com.laptrinhjavaweb.dto.SanPhamDTO;
-import com.laptrinhjavaweb.dto.SanPhamHinhAnhDTO;
-import com.laptrinhjavaweb.dto.ThuocTinhDTO;
 import com.laptrinhjavaweb.entity.SanPhamEntity;
 import com.laptrinhjavaweb.repository.SanPhamRepository;
 import com.laptrinhjavaweb.service.IBienTheService;
@@ -52,7 +53,11 @@ public class SanPhamServiceImpl implements ISanPhamService{
 	@Override
 	@Transactional
 	public SanPhamDTO save(SanPhamDTO sanPhamDTO) {
-		SanPhamEntity sanPhamEntity = sanPhamConvert.convertToEntity(sanPhamDTO);
+		SanPhamEntity sanPhamEntity = sanPhamRepository.findBySlug(sanPhamDTO.getSlug());
+		if(sanPhamEntity != null){
+			return null;
+		}
+		sanPhamEntity = sanPhamConvert.convertToEntity(sanPhamDTO);
 		sanPhamRepository.save(sanPhamEntity);
 		sanPhamDTO.setId(sanPhamEntity.getId());
 		List<GiaTriThuocTinhDTO> newListGiaTriThuocTinhDTO = new ArrayList<>();
@@ -89,8 +94,28 @@ public class SanPhamServiceImpl implements ISanPhamService{
 	@Override
 	public SanPhamDTO findBySlug(String slug) {
 		SanPhamEntity sanPhamEntity = sanPhamRepository.findBySlug(slug);
+		if(sanPhamEntity == null){
+			return null;
+		}
 		SanPhamDTO sanPhamDTO = sanPhamConvert.convertToDTO(sanPhamEntity);
 		return sanPhamDTO;
+	}
+
+	@Override
+	public Map<String, Object> pageAble(Integer pageCurrent, Integer limit) {
+		Pageable pageable = PageRequest.of(pageCurrent - 1, limit);
+		Page<SanPhamEntity> page = sanPhamRepository.findAll(pageable);
+		List<SanPhamDTO> listSanPhamDTO = page.getContent().stream().map(
+				item -> sanPhamConvert.convertToDTO(item)
+		).collect(Collectors.toList());
+		PageAbleDTO pageAbleDTO = new PageAbleDTO();
+		pageAbleDTO.setPagecurrent(pageCurrent);
+		pageAbleDTO.setTotalpage(page.getTotalPages());
+		pageAbleDTO.setPagefirst(1);
+		Map<String, Object> results = new HashMap<>();
+		results.put("data", listSanPhamDTO);
+		results.put("meta", pageAbleDTO);
+		return results;
 	}
 
 }
